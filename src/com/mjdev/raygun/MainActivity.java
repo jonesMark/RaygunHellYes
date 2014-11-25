fc64298d0c2261c7577907336b886ff4670da6e9
James Teague <jtteague13@gmail.com>
2014-11-25 09:45:44 -0600

    added xml stuff

--- src/com/mjdev/raygun/MainActivity.java
+++ src/com/mjdev/raygun/MainActivity.java
@@ -1,18 +1,19 @@
 package com.mjdev.raygun;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 //Urness Assignments:
@@ -30,15 +31,17 @@ public class MainActivity extends Activity{
 	ArrayList<String> description = new ArrayList<String>();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		File list = new File("list.txt");
-		try {
-			Scanner scanner = new Scanner(list);
-			readInfo(scanner);
-		} catch(FileNotFoundException e){
-			e.printStackTrace();
-		}
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		try {
+			parseXmlFile();
+		} catch (XmlPullParserException e) {
+			Log.v("Time Elements", e.toString());
+		} catch (IOException e) {
+			Log.v("Time Elements", e.toString());
+		}
 		//tabs
 		ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -79,27 +82,27 @@ public class MainActivity extends Activity{
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	private void readInfo(Scanner s) {
-		//pull first num, numer of iterations
-		int numOfItems = s.nextInt();
-		while(s.hasNext()){
-			//pull title
-			titles.add(s.nextLine());
-			s.nextLine();
-			System.out.println(s.nextLine());//0
-			boolean go = true;
-			while (go) {
-				String check = s.nextLine();
-				if (Integer.parseInt(check)==0) {
-					go = false;
-				}
-				else {
-					description.add(check);
-				}
-			}
-
-
-		}
+	
+	private void parseXmlFile() throws XmlPullParserException, IOException{
+		XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        factory.setNamespaceAware(true);
+        XmlPullParser xpp = factory.newPullParser();
+        xpp.setInput(getAssets().open("list.xml"),null);
+        int eventType = xpp.getEventType();
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            if(eventType == XmlPullParser.START_DOCUMENT) {
+                Log.v("Time Elements", "Start document");
+            } else if(eventType == XmlPullParser.END_DOCUMENT) {
+            	Log.v("Time Elements", "End document");
+            } else if(eventType == XmlPullParser.START_TAG) {
+            	Log.v("Time Elements","Start tag "+xpp.getName());
+            } else if(eventType == XmlPullParser.END_TAG) {
+            	Log.v("Time Elements","End tag "+xpp.getName());
+            } else if(eventType == XmlPullParser.TEXT) {
+            	Log.v("Time Elements","Text "+xpp.getText());
+            }
+            eventType = xpp.next();
+           }
 	}
 
 

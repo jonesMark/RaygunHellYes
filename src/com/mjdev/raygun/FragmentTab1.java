0f0d2176300c32132f1a9aa1c6d6ad0bf9fb928a
mjones2930 <markwjonesii@gmail.com>
2014-12-10 22:31:05 -0600

    It works! Array list for main page as custom backgrounds for each array.  For prototype, listener disabled due to NULL errors

--- src/com/mjdev/raygun/FragmentTab1.java
+++ src/com/mjdev/raygun/FragmentTab1.java
@@ -1,33 +1,21 @@
 package com.mjdev.raygun;
 
 
+import java.util.ArrayList;
+
+import junit.framework.Assert;
+import android.annotation.SuppressLint;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-
-
-import junit.framework.Assert;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -35,38 +23,25 @@ import android.widget.TextView;
 public class FragmentTab1 extends Fragment {
 	ListView listView;
 	int num = 1;
-	String[] text = { "I NEED A VACA", "GIVE ME A DOLLA", "PARTY!!!", "WHO NEES SLEEP?", "Five", "Six", "Seven",
-			"Eight", "Nine", "Ten" };
-	/*	ListView listview;
-	int num = 1; 
-	String[] text = { "I NEED A VACA", "GIVE ME A DOLLA", "PARTY!!!", "WHO NEES SLEEP?", "Five", "Six", "Seven",
-			"Eight", "Nine", "Ten" };
-
-	int[] image = { R.drawable.one, R.drawable.two, R.drawable.three,
-			R.drawable.four, R.drawable.five, R.drawable.six, R.drawable.seven,
-			R.drawable.eight, R.drawable.nine, R.drawable.ten };
-	int textlength = 0;
-	ArrayList<String> text_sort = new ArrayList<String>();
-	ArrayList<Integer> image_sort = new ArrayList<Integer>();
-	
-			listview = (ListView) findViewById(R.id.ListView01);
-		listview.setAdapter(new MyCustomAdapter(text, image));
-
-*/
+	String[] text;
 	  public View onCreateView(LayoutInflater inflater, ViewGroup container, 
 	                           Bundle savedInstanceState){
 		View view = inflater.inflate(R.layout.frag1, container, false);
+		System.out.print("I AM RUNNING");
+		Log.d("TAG", "Message");
 		view.setClickable(true);
 		ListView listView = (ListView) view.findViewById(R.id.ListView01);
-		//listView.setAdapter(new MyCustomAdapter(text));
 		listView.setOnItemClickListener(myClickListener);
+
+		text = getResources().getStringArray(R.array.bucket_list);
+		listView.setAdapter(new MyCustomAdapter(text));
+		
 		return view;
 	  }
 		class MyCustomAdapter extends BaseAdapter
 		{
 
 			String[] data_text;
-			//int[] data_image;
 
 			MyCustomAdapter()
 			{
@@ -76,17 +51,14 @@ public class FragmentTab1 extends Fragment {
 			MyCustomAdapter(String[] text)
 			{
 				data_text = text;
-				//data_image = image;
 			}
 			MyCustomAdapter(ArrayList<String> text)
 			{
 				data_text = new String[text.size()];
-				//data_image = new int[image.size()];
 
 				for(int i=0;i<text.size();i++)
 				{
 					data_text[i] = text.get(i);
-					//data_image[i] = image.get(i);
 				}
 
 			}
@@ -106,35 +78,25 @@ public class FragmentTab1 extends Fragment {
 				return position;
 			}
 
-			@SuppressLint("NewApi")
+			@SuppressLint({ "NewApi", "ViewHolder" })
 			public View getView(int position, View convertView, ViewGroup parent)
 			{
 				
 				LayoutInflater inflater = getActivity().getLayoutInflater();
 				View row;
 				row = inflater.inflate(R.layout.listview, parent, false);
-				//String back = Integer.toString(num);
-				//row.setBackground(R.id.(Integer.toString(num)));
-				TextView textview = (TextView) row.findViewById(R.id.TextView01);
-				//ImageView imageview = (ImageView) row.findViewById(R.id.ImageView01);
-				//imageview.setVisibility(imageview.INVISIBLE);
-				//getResources().getIdentifier(imagename, "drawable", this.getPackageName()
-				
+				TextView textview = (TextView) row.findViewById(R.id.TextView01);				
 				
 				
 				textview.setText(data_text[position]);
-				//textview.setBackground(getResources().getDrawable(R.drawable.eight));
-				//().getDrawable(R.drawable.ready)
+				textview.setBackgroundColor(0xB3FFFFFF);
 				String imagename = "i" + num;
 				num ++;
 				if (num == 4) {
 					num = 1;
 				}
-				//row.setBackground(getResources().getIdentifier(imagename, "drawable", null ));
-				//row.setBackground(getDrawable(getApplicationContext(), imagename));			
-				row.setBackground(getResources().getDrawable(getDrawables( getActivity().getBaseContext() , imagename)));
 
-				//imageview.setImageResource(data_image[position]);
+				row.setBackground(getResources().getDrawable(getDrawables( getActivity().getBaseContext() , imagename)));
 
 				return (row);
 
@@ -148,13 +110,17 @@ public class FragmentTab1 extends Fragment {
 	        return context.getResources().getIdentifier(name,
 	                "drawable", context.getPackageName());
 	    }
+	    
+	    
 	  public OnItemClickListener myClickListener = new OnItemClickListener() {
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 			MainActivity act = (MainActivity) getActivity();
-			String entry = (String) parent.getItemAtPosition(position);
+			Log.d("message01", Integer.toString(position));
+			//String entry = (String) parent.getItemAtPosition(position);
+			Log.d("killme","dummy message");
             Intent intent = new Intent(view.getContext(), TaskDescription.class);
-            intent.putExtra("task", entry);
+            intent.putExtra("task", "Zoo Brew");
         	intent.putStringArrayListExtra("titles", act.titles);
         	intent.putStringArrayListExtra("descriptions", act.description);
             startActivity(intent);
